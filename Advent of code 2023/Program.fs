8a1f4c1100a3465cfd47ffab3d8f1854a6edadf9
zaneta.severova <severova@cngroup.dk>
2023-12-04 15:27:35 +0100

    day4

--- Advent of code 2023/Program.fs
+++ Advent of code 2023/Program.fs
@@ -1,21 +1,21 @@
 ﻿open System
 open Advent_of_code_2023
-open Advent_of_code_2023.Helpers
 
 let fileNames =
     let d = DateTime.Now.Day
-    $"day{d}a", $"day{d}b"
+    $"day{d}a", $"day{d}b" 
+    //"day3a", "day3b"
 
 [<EntryPoint>]
 let main argv =
     let filenameEasy, fileNameHard = fileNames
     
-    let first =
-        InputHandler.readFile fileNameHard 
-        |> day2.firstPart
+    // let first =
+    //     InputHandler.readFile fileNameHard 
+    //     |> day4.firstPart
         
     let second =
         InputHandler.readFile fileNameHard 
-        |> day2.secondPart
-    printfn "%A, %A" first second
+        |> day4.secondPart
+    printfn "%A, %A" 0 second
     0 // return an integer exit code
---
+++ Advent of code 2023/day4.fs
@@ -0,0 +1,60 @@
+﻿module Advent_of_code_2023.day4
+open System
+
+let private processLine(line: string) =
+    let colon = line.IndexOf(":")
+    let splitLine =
+        line.Substring(colon+1).Split("|", StringSplitOptions.RemoveEmptyEntries)
+        |> Seq.map(fun f -> f.Split(" ", StringSplitOptions.RemoveEmptyEntries) |> Seq.map int)
+        
+    let winningNumbers, mine = splitLine |> Seq.head |> Set.ofSeq, splitLine |> Seq.last |> Set.ofSeq
+    let mineWinning = Set.intersect winningNumbers mine |> Set.toList
+    mineWinning
+
+let firstPart(data: string seq) =
+    let res =
+        data
+        |> Seq.map processLine
+        |> Seq.map(fun f -> f.Length)
+        |> Seq.map(fun f -> if f = 0 then 0.0
+                            else
+                                let e = f-1
+                                Math.Pow(2, e))
+        |> Seq.sum        
+    res
+    
+let secondPart(data: string seq) =
+    let winnings =
+        data
+        |> Seq.map processLine
+        |> Seq.map(fun f -> f.Length)
+        |> Seq.indexed 
+        |> Map.ofSeq
+    
+    let getMoreCards (state: Map<int, int>) (index: int) =
+        let eval = winnings |> Map.find index
+        let copies = state |> Map.find index
+        
+        let indices = [index+1..index+eval]
+        
+        let addCopies(st: Map<int, int>) i =
+            st |> Map.change i (fun (Some c) -> c+copies |> Some)
+            
+        let newState =
+            indices
+            |> Seq.fold addCopies state
+        
+        newState
+        
+    let initState =
+        winnings |> Map.toSeq |> Seq.map(fun (i, _) -> i, 1) |> Map.ofSeq
+        
+    let res =
+        winnings
+        |> Map.toSeq
+        |> Seq.map fst
+        |> Seq.fold getMoreCards (initState)
+        |> Map.toSeq
+        |> Seq.sumBy snd
+        
+    res 
