942a02f6b8b0b8c167c4c8e17573e2e35d351aeb
zaneta.severova <severova@cngroup.dk>
2023-12-06 12:58:39 +0100

    day6 + refactoring

---
+++ Advent of code 2023/Tests.fs
@@ -0,0 +1,154 @@
+﻿module Advent_of_code_2023.Tests
+
+open NUnit.Framework
+open System
+    
+    
+[<Test>]
+let ``Day 1 first part`` () =
+    let fileName = "day1a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day1.firstPart
+    
+    Assert.AreEqual(result, 142)
+
+[<Test>]    
+let ``Day 1 second part`` () =
+    let fileName = "day1a2"
+    let result =
+        InputHandler.readFile fileName 
+        |> day1.secondPart
+    
+    Assert.AreEqual(result, 281)
+    
+[<Test>]
+let ``Day 2 first part`` () =
+    let fileName = "day2a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day2.firstPart
+    
+    Assert.AreEqual(result, 8)
+
+[<Test>]    
+let ``Day 2 second part`` () =
+    let fileName = "day2a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day2.secondPart
+    
+    Assert.AreEqual(result, 2286)
+    
+[<Test>]
+let ``Day 3 first part`` () =
+    let fileName = "day3a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day3.firstPart
+    
+    Assert.AreEqual(result, 4361)
+    
+[<Test>]
+let ``Day 3 second part`` () =
+    let fileName = "day3a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day3.secondPart
+    
+    Assert.AreEqual(result, 467835)
+    
+[<Test>]
+let ``Day 4 first part`` () =
+    let fileName = "day4a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day4.firstPart
+    
+    Assert.AreEqual(result, 13)
+    
+[<Test>]
+let ``Day 4 second part`` () =
+    let fileName = "day4a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day4.secondPart
+    
+    Assert.AreEqual(result, 30)
+    
+[<Test>]
+let ``Day 5 first part`` () =
+    let fileName = "day5a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day5.firstPart 
+    
+    Assert.AreEqual(result, 35)
+    
+[<Test>]
+let ``Day 5 second part`` () =
+    let fileName = "day5a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day5.secondPart 
+    
+    Assert.AreEqual(result, 46)
+
+
+[<Test>]    
+let ``Day 6 first part`` () =
+    let fileName = "day6a"
+    let result =
+     InputHandler.readFile fileName 
+     |> day6.firstPart 
+
+    Assert.AreEqual(result, 288)
+    
+[<Test>]
+let ``Day 6 second part`` () =
+    let fileName = "day6a"
+    let result =
+        InputHandler.readFile fileName 
+        |> day6.secondPart 
+    
+    Assert.AreEqual(result, 71503)
+    
+[<Test>]    
+let ``Day 7 first part`` () =
+    let fileName = "day7a"
+    // let result =
+    //     InputHandler.readFile fileName 
+    //     |> day5.firstPart 
+    
+    //Assert.AreEqual(result, ()) //TODO
+    Assert.Fail("Not implemented")
+    
+[<Test>]
+let ``Day 7 second part`` () =
+    let fileName = "day7a"
+    // let result =
+    //     InputHandler.readFile fileName 
+    //     |> day5.secondPart 
+    
+    //Assert.AreEqual(result, ()) //TODO
+    Assert.Fail("Not implemented")
+    
+[<Test>]    
+let ``Day 8 first part`` () =
+    let fileName = "day8a"
+    // let result =
+    //     InputHandler.readFile fileName 
+    //     |> day5.firstPart 
+    
+    //Assert.AreEqual(result, ()) //TODO
+    Assert.Fail("Not implemented")
+    
+[<Test>]
+let ``Day 8 second part`` () =
+    let fileName = "day8a"
+    // let result =
+    //     InputHandler.readFile fileName 
+    //     |> day5.secondPart 
+    
+    //Assert.AreEqual(result, ()) //TODO
+    Assert.Fail("Not implemented")
---
+++ Advent of code 2023/day6.fs
@@ -0,0 +1,40 @@
+﻿module Advent_of_code_2023.day6
+
+open System
+
+let private parseGames(lines: int64 seq seq) =
+    Seq.zip (lines |> Seq.head) (lines |> Seq.last)
+    
+let private processGame(time: int64, currentRecord: int64) =
+    let startIndex, endIndex = (time / 2L), time-1L
+    
+    let calculateDistance (holdingButtonMs: int64) = (time - holdingButtonMs) * holdingButtonMs
+    
+    let scores =
+        seq {1L..endIndex}
+        |> Seq.map calculateDistance
+        |> Seq.where(fun f -> f > currentRecord)
+        |> Seq.length
+        
+    scores
+    
+let firstPart(data: string seq) =
+    let games =
+        data
+        |> Seq.map(fun line -> (line.Split(":")[1]).Split(" ", StringSplitOptions.RemoveEmptyEntries) |> Seq.map int64)
+        |> parseGames
+        |> Seq.map processGame
+        //|> Seq.toList
+        |> Seq.fold(*) 1
+        
+    games
+
+let secondPart(data: string seq) =
+    let game =
+        data
+        |> Seq.map(fun line -> (line.Split(":")[1]).Replace(" ", "") |> int64)
+        |> Seq.toList
+        
+    let res = processGame (game[0], game[1])
+        
+    res
